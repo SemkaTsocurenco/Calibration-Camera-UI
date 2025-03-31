032a3a94d924ffcc9b268029d19855ed7969fe7c
Семён Цокуренко <svtsokurenko@kamaz.ru>
2025-03-31 18:20:57 +0300

    изменение название переменных

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QComboBox>
+#include <QPushButton>
 #include <QLabel>
 #include <QTimer>
 #include <QVBoxLayout>
@@ -13,31 +14,30 @@ class MainWindow : public QWidget {
     Q_OBJECT
 public:
     MainWindow(QWidget *parent = nullptr) : QWidget(parent), cap(0) {
-        // Инициализация элементов интерфейса
-        comboBox = new QComboBox(this);
-        // Добавляем несколько вариантов камер (индексы 0, 1, 2, 3)
-        comboBox->addItem("Камера 0", 0);
-        comboBox->addItem("Камера 1", 1);
-        comboBox->addItem("Камера 2", 2);
-        comboBox->addItem("Камера 3", 3);
 
-        videoLabel = new QLabel(this);
-        videoLabel->setFixedSize(640, 480); // Размер отображаемого видео
+        // Инициализируем все элементы 
+        cap_select = new QComboBox(this);
+        cap_select->addItem("Камера 0", 0);
+        cap_select->addItem("Камера 1", 1);
+        cap_select->addItem("Камера 2", 2);
+        cap_select->addItem("Камера 3", 3);
 
-        QVBoxLayout *layout = new QVBoxLayout(this);
-        layout->addWidget(comboBox);
-        layout->addWidget(videoLabel);
-        setLayout(layout);
+        videoLabel = new QLabel;
+        videoLabel->setFixedSize(640, 480);
 
-        // При смене выбранного элемента выпадающего списка вызываем слот смены камеры
-        connect(comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(onCameraChanged(int)));
+        QHBoxLayout *horisontal_cam_and_select = new QHBoxLayout (this);
+        horisontal_cam_and_select->addWidget(cap_select);
+        horisontal_cam_and_select->addWidget(videoLabel);
 
-        // Таймер для регулярного обновления кадра с камеры
+        // Добавляем обработку смены камеры 
+        connect(cap_select, SIGNAL(currentIndexChanged(int)), this, SLOT(onCameraChanged(int)));
+
+        // Обнавление кадра по таймеру 
         timer = new QTimer(this);
         connect(timer, SIGNAL(timeout()), this, SLOT(updateFrame()));
-        timer->start(30); // Интервал ~30 мс (примерно 30 FPS)
+        timer->start(30); // 30 мс (~ 30 FPS)
+    
     }
-
     ~MainWindow() {
         if (cap.isOpened()) {
             cap.release();
@@ -45,32 +45,29 @@ public:
     }
 
 public slots:
-    // Слот для обновления кадра и отображения его в QLabel
-    void updateFrame() {
-        if (cap.isOpened()) {
-            cv::Mat frame;
-            cap >> frame;
-            if (!frame.empty()) {
-                // Конвертируем из BGR (формат по умолчанию в OpenCV) в RGB
-                cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
-                QImage qimg(frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888);
-                videoLabel->setPixmap(QPixmap::fromImage(qimg));
-            }
-        }
-    }
 
-    // Слот для смены устройства камеры
-    void onCameraChanged(int index) {
-        int device = comboBox->itemData(index).toInt();
-        if (cap.isOpened()) {
+    void onCameraChanged(int index){
+        int device = cap_select->itemData(index).toInt();
+        if (cap.isOpened()){
             cap.release();
         }
         cap.open(device);
     }
 
+    void updateFrame(){
+        cv::Mat frame;
+        cap >> frame;
+          if (!frame.empty()) {
+            cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
+            QImage qimg(frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888);
+            videoLabel->setPixmap(QPixmap::fromImage(qimg));
+        }
+    }
+
 private:
     cv::VideoCapture cap;
-    QComboBox *comboBox;
+    QComboBox *cap_select;
+    QPushButton *button_go_calibrate;
     QLabel *videoLabel;
     QTimer *timer;
 };
